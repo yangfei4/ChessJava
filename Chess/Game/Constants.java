03a4da833388f8017eea56bcbcb0779a8f137bc1
D-YF <daiyf1028@gmail.com>
2023-05-28 17:33:41 -0500

    Constant variables

---
+++ Chess/Game/Constants.java
@@ -0,0 +1,14 @@
+package Game;
+
+public class Constants {
+    public static final int BOARD_SIZE = 8;
+    public static final int WHITE = 0;
+    public static final int BLACK = 1;
+    
+    public static final int PAWN_ID = 0;
+    public static final int ROOK_ID = 1;
+    public static final int KNIGHT_ID = 2;
+    public static final int BISHOP_ID = 3;
+    public static final int QUEEN_ID = 4;
+    public static final int KING_ID = 5;
+}
