a62f635c55252a0a47e920a9f369a13a122ae9fa
D-YF <daiyf1028@gmail.com>
2023-06-11 23:10:36 -0500

    added hasPiece and setpiece methods

--- Chess/Boards/Board.java
+++ Chess/Boards/Board.java
@@ -1,7 +1,5 @@
 package Boards;
 
-import java.util.*;
-
 import Game.*;
 import Pieces.*;
 
@@ -24,41 +22,45 @@ public class Board {
             int row1 = (color==WHITE)?7:0;
             int row2 = (color==WHITE)?6:1;
             // Rook
-            this.board[row1][0] = new Spot(new Position(row1, 0), Optional.of(new Rook(color)));
-            this.board[row1][7] = new Spot(new Position(row1, 7), Optional.of(new Rook(color)));
+            this.board[row1][0] = new Spot(new Rook(color));
+            this.board[row1][7] = new Spot(new Rook(color));
             // Knight
-            this.board[row1][1] = new Spot(new Position(row1, 1), Optional.of(new Knight(color)));
-            this.board[row1][6] = new Spot(new Position(row1, 6), Optional.of(new Knight(color)));
+            this.board[row1][1] = new Spot(new Knight(color));
+            this.board[row1][6] = new Spot(new Knight(color));
             // Bishop
-            this.board[row1][2] = new Spot(new Position(row1, 2), Optional.of(new Bishop(color)));
-            this.board[row1][5] = new Spot(new Position(row1, 5), Optional.of(new Bishop(color)));
+            this.board[row1][2] = new Spot(new Bishop(color));
+            this.board[row1][5] = new Spot(new Bishop(color));
             // Queen
-            this.board[row1][3] = new Spot(new Position(row1, 3), Optional.of(new Queen(color)));
+            this.board[row1][3] = new Spot(new Queen(color));
             // King
-            this.board[row1][4] = new Spot(new Position(row1, 4), Optional.of(new King(color)));
+            this.board[row1][4] = new Spot(new King(color));
             // Pawn
             for(int y=0; y<8; y++)
-                this.board[row2][y] = new Spot(new Position(row2, y), Optional.of(new Pawn(color)));
+                this.board[row2][y] = new Spot(new Pawn(color));
         }
 
         for(int x=2; x<=5; x++){
             for(int y=0; y<8; y++){
-                this.board[x][y] = new Spot(new Position(x, y), Optional.empty());
+                this.board[x][y] = new Spot(null);
             }
         }
 
     }
 
-    public Spot getSpot(int x, int y){ 
+    public Piece getPiece(int x, int y){ 
         if(x<0 || y<0 || x>=BOARD_SIZE || y>=BOARD_SIZE){
             throw new IndexOutOfBoundsException("Index out of bound");
         }
 
-        return this.board[x][y];
+        return this.board[x][y].getPiece();
     }
 
-    public void setSpot(int x, int y, Spot s){
-        this.board[x][y] = s;
+    public void setPiece(int x, int y, Piece p){
+        this.board[x][y].setPiece(p);
+    }
+
+    public boolean hasPiece(int x, int y){
+        return board[x][y].hasPiece();
     }
 
 }
