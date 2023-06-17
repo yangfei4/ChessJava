70f49046acc0a1bc48027f2750c0c9a50e311240
D-YF <daiyf1028@gmail.com>
2023-06-16 23:33:20 -0500

    added all methods to check checkmate and king-underattacking situation

--- Chess/Boards/Board.java
+++ Chess/Boards/Board.java
@@ -2,11 +2,17 @@ package Boards;
 
 import Game.*;
 import Pieces.*;
+import java.util.*;
 
 public class Board {
     public int BOARD_SIZE = Constants.BOARD_SIZE;
     public int WHITE = Constants.WHITE;
     public int BLACK = Constants.BLACK;
+
+    private int whiteKingRow; // Row position of the white king
+    private int whiteKingCol; // Column position of the white king
+    private int blackKingRow; // Row position of the black king
+    private int blackKingCol; // Column position of the black king    
 
     private Spot[][] board;
 
@@ -34,6 +40,8 @@ public class Board {
             this.board[row1][3] = new Spot(new Queen(color));
             // King
             this.board[row1][4] = new Spot(new King(color));
+            // Set King's position
+            setKingPosition(color, row1, 4);
             // Pawn
             for(int y=0; y<8; y++)
                 this.board[row2][y] = new Spot(new Pawn(color));
@@ -44,7 +52,6 @@ public class Board {
                 this.board[x][y] = new Spot(null);
             }
         }
-
     }
 
     public Piece getPiece(int x, int y){ 
@@ -63,4 +70,169 @@ public class Board {
         return board[x][y].hasPiece();
     }
 
+    public boolean isSquareUnderAttack(int row, int col, int attackingColor) {
+        // Check if the specified square is under attack by any opponent piece
+        // Check for Rooks and Queens attacking horizontally or vertically
+        int[][] rookDirections = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int[] direction : rookDirections) {
+            int x = row + direction[0];
+            int y = col + direction[1];
+    
+            while (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE) {
+                Piece piece = getPiece(x, y);
+                if (piece != null) {
+                    if (piece.getColor()!= attackingColor && (piece instanceof Rook || piece instanceof Queen)) {
+                        System.out.println("King is under attack!");
+                        return true; // Square is under attack by a Rook or Queen
+                    } else {
+                        break; // Path is blocked by another piece
+                    }
+                }
+                x += direction[0];
+                y += direction[1];
+            }
+        }
+    
+        // Check for Bishops and Queens attacking diagonally
+        int[][] bishopDirections = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+        for (int[] direction : bishopDirections) {
+            int x = row + direction[0];
+            int y = col + direction[1];
+    
+            while (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE) {
+                Piece piece = getPiece(x, y);
+                if (piece != null) {
+                    if (piece.getColor() != attackingColor && (piece instanceof Bishop || piece instanceof Queen)) {
+                        System.out.println("King is under attack!");
+                        return true; // Square is under attack by a Bishop or Queen
+                    } else {
+                        break; // Path is blocked by another piece
+                    }
+                }
+                x += direction[0];
+                y += direction[1];
+            }
+        }
+    
+        // Check for Knights attacking
+        int[][] knightMoves = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
+        for (int[] move : knightMoves) {
+            int x = row + move[0];
+            int y = col + move[1];
+    
+            if (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE) {
+                Piece piece = getPiece(x, y);
+                if (piece != null && piece.getColor() != attackingColor && piece instanceof Knight) {
+                        System.out.println("King is under attack!");
+                        return true; // Square is under attack by a Knight
+                }
+            }
+        }
+    
+        // Check for Pawns attacking
+        int pawnDirection = (attackingColor == WHITE) ? 1 : -1;
+        int[][] pawnMoves = {{pawnDirection, -1}, {pawnDirection, 1}};
+        for (int[] move : pawnMoves) {
+            int x = row + move[0];
+            int y = col + move[1];
+    
+            if (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE) {
+                Piece piece = getPiece(x, y);
+                if (piece != null && piece.getColor() != attackingColor && piece instanceof Pawn) {
+                        System.out.println("King is under attack!");
+                        return true; // Square is under attack by a Pawn
+                }
+            }
+        }
+    
+        // Check for Kings attacking
+        int[][] kingMoves = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        for (int[] move : kingMoves) {
+            int x = row + move[0];
+            int y = col + move[1];
+
+            if (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE) {
+                Piece piece = getPiece(x, y);
+                if (piece != null && piece.getColor() != attackingColor && piece instanceof King) {
+                    System.out.println("King is under attack!");
+                    return true; // Square is under attack by a King
+                }
+            }
+        }
+
+        return false; // Square is not under attack by any opponent piece
+    }
+
+    public void setKingPosition(int color, int row, int col) {
+        if (color == WHITE) {
+            whiteKingRow = row;
+            whiteKingCol = col;
+            System.out.println("WHITE KING was updated to");
+            System.out.println(row);
+            System.out.println(col);
+        } else {
+            blackKingRow = row;
+            blackKingCol = col;
+            System.out.println("BLACK KING was updated to");
+            System.out.println(row);
+            System.out.println(col);
+        }
+    }
+
+    public int[] getKingPosition(int color) {
+        if (color == WHITE) {
+            return new int[]{whiteKingRow, whiteKingCol};
+        } else {
+            return new int[]{blackKingRow, blackKingCol};
+        }
+    }
+
+    public boolean isChecked(int color) {
+        int[] kingPos = getKingPosition(color);
+        int kingRow = kingPos[0];
+        int kingCol = kingPos[1];
+    
+        // Check if the king is under attack
+        return isSquareUnderAttack(kingRow, kingCol, color);
+    }
+
+    public List<int[][]> getAllValidMoves(int color) {
+        List<int[][]> validMoves = new ArrayList<>();
+    
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                Piece piece = getPiece(row, col);
+    
+                if (piece != null && piece.getColor() == color) {
+                    List<int[][]> pieceValidMoves = piece.getValidMoves(row, col, this);
+    
+                    for (int[][] move : pieceValidMoves) {
+                        // 尝试移动棋子并检查是否仍然被将军
+                        int startRow = move[0][0];
+                        int startCol = move[0][1];
+                        int endRow = move[1][0];
+                        int endCol = move[1][1];
+    
+                        Piece capturedPiece = getPiece(endRow, endCol);
+    
+                        // 尝试移动棋子
+                        setPiece(endRow, endCol, piece);
+                        setPiece(startRow, startCol, null);
+    
+                        if (!isChecked(color)) {
+                            // 移动有效，添加到有效移动列表中
+                            validMoves.add(move);
+                        }
+    
+                        // 恢复棋盘状态
+                        setPiece(startRow, startCol, piece);
+                        setPiece(endRow, endCol, capturedPiece);
+                    }
+                }
+            }
+        }
+    
+        return validMoves;
+    }
+    
 }
