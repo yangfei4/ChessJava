d372986e24b6959b4eff0bca20a748cc35d11d00
D-YF <daiyf1028@gmail.com>
2023-06-16 23:37:49 -0500

    implement moving rules for Queen

--- Chess/Pieces/Queen.java
+++ Chess/Pieces/Queen.java
@@ -1,15 +1,12 @@
 package Pieces;
 
-import Game.*;
-
+import Boards.Board;
+import java.util.*;
 
 public class Queen extends Piece{
-    // TODOs: add special attribute for different pieces
-    public int QUEEN_ID = Constants.QUEEN_ID;
 
     public Queen(int color){
         super(color);
-        this.pieceID = QUEEN_ID;
     }
 
     @Override
@@ -19,9 +16,93 @@ public class Queen extends Piece{
 
 
     @Override
-    public boolean isLegitMove(int startx, int starty, int endx, int endy){
+    public boolean isLegitMove(int[][] moveCoordinates, Board board){
+        int startRow = moveCoordinates[0][0];
+        int startCol = moveCoordinates[0][1];
+        int endRow = moveCoordinates[1][0];
+        int endCol = moveCoordinates[1][1];
+    
+        // Check if the move is a valid diagonal move
+        if (Math.abs(startRow - endRow) == Math.abs(startCol - endCol)) {
+            int rowDirection = (endRow - startRow) / Math.abs(endRow - startRow);
+            int colDirection = (endCol - startCol) / Math.abs(endCol - startCol);
+    
+            int row = startRow + rowDirection;
+            int col = startCol + colDirection;
+    
+            while (row != endRow && col != endCol) {
+                if (board.hasPiece(row, col)) {
+                    return false; // Path is blocked
+                }
+                row += rowDirection;
+                col += colDirection;
+            }
+    
+            return true;
+        }
+    
+        // Check if the move is a valid straight move
+        if (startRow == endRow || startCol == endCol) {
+            int rowDirection = 0;
+            int colDirection = 0;
+    
+            if (startRow != endRow) {
+                rowDirection = (endRow - startRow) / Math.abs(endRow - startRow);
+            }
+            if (startCol != endCol) {
+                colDirection = (endCol - startCol) / Math.abs(endCol - startCol);
+            }
+    
+            int row = startRow + rowDirection;
+            int col = startCol + colDirection;
+    
+            while (row != endRow || col != endCol) {
+                if (board.hasPiece(row, col)) {
+                    return false; // Path is blocked
+                }
+                row += rowDirection;
+                col += colDirection;
+            }
+    
+            return true;
+        }
+    
+        return false;
+    }
+
+    @Override
+    public void setMoved(){
         
-        return true;
+    }
+
+    @Override
+    public List<int[][]> getValidMoves(int startRow, int startCol, Board board) {
+        List<int[][]> validMoves = new ArrayList<>();
+        // Possible movement directions for a queen (diagonal and straight)
+        int[][] directions = {
+                {-1, -1}, {-1, 0}, {-1, 1},
+                {0, -1},           {0, 1},
+                {1, -1},  {1, 0},  {1, 1}
+        };
+
+        for (int[] direction : directions) {
+            int row = startRow + direction[0];
+            int col = startCol + direction[1];
+
+            while (row >= 0 && row < 8 && col >= 0 && col < 8) {
+                int[][] moveCoordinates = {{startRow, startCol}, {row, col}};
+                if (isLegitMove(moveCoordinates, board)) {
+                    validMoves.add(moveCoordinates);
+                } else {
+                    break;
+                }
+
+                row += direction[0];
+                col += direction[1];
+            }
+        }
+
+        return validMoves;
     }
 
 }
